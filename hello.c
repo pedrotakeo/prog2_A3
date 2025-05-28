aa37df1e0804ff815ed5ad59cf0b1aa5e083fc9c
pedrotakeo <pts24@inf.ufpr.br>
2025-05-28 01:39:59 -0300

    added movement features

--- hello.c
+++ hello.c
@@ -5,8 +5,8 @@
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro_primitives.h>
 
-#define KEY_SEEN     1
-#define KEY_DOWN     2
+#define RIGHT     1
+#define LEFT     2
 
 void must_init(bool test, const char *description)
 {
@@ -22,6 +22,7 @@ typedef struct player{
     int og_dimensions;
     int dimensions;
     int speed;
+    int direction;
 
     int shadow_x;
     int shadow_y;
@@ -36,8 +37,15 @@ int main(){
     player player;
     int screen_x = 1280;
     int screen_y = 720;
-    float level_height = screen_y - 150;
+    float level_height = screen_y - 175;
     float frame = 0;
+    int alternate_frame = 2;
+    int iteration = 2;
+    int sprite_off_x = 0;
+    int sprite_off_y = 64;
+    int background_slider = 0;
+    
+
     must_init(al_init(), "allegro");
     must_init(al_install_keyboard(), "keyboard");
 
@@ -69,11 +77,12 @@ int main(){
 
     al_convert_mask_to_alpha(player.sprite, al_map_rgb(26, 255, 0));
 
-    player.x = 100;
+    player.x = 200;
     player.y = level_height;
     player.og_dimensions = 64;
     player.dimensions = 100;
-    player.speed = 30;
+    player.speed = 20;
+    player.direction = RIGHT;
 
     int shadow_height = 7;
     int shadow_mod = 0;
@@ -94,7 +103,14 @@ int main(){
 
     al_start_timer(timer);
     while(1)
-    {
+    {   
+        sprite_off_x = 0;
+        if(player.direction == RIGHT){
+            sprite_off_y = 64;
+        }
+        else{
+            sprite_off_y = 0;
+        }
         al_wait_for_event(queue, &event);
 
         switch(event.type)
@@ -103,15 +119,50 @@ int main(){
             case ALLEGRO_EVENT_TIMER:
                 al_get_keyboard_state(&ks);
 
-                if(player.x >= player.speed && (al_key_down(&ks, ALLEGRO_KEY_LEFT) || al_key_down(&ks, ALLEGRO_KEY_A)))
-                    player.x -= player.speed;
-                if(player.x <= (screen_x - player.speed - player.dimensions) && (al_key_down(&ks, ALLEGRO_KEY_RIGHT) || al_key_down(&ks, ALLEGRO_KEY_D)))
-                    player.x += player.speed;
+                if(player.x >= player.speed && (al_key_down(&ks, ALLEGRO_KEY_LEFT) || al_key_down(&ks, ALLEGRO_KEY_A))){
+
+                    if(player.x > 100 || background_slider == 0){
+                        player.x -= player.speed;
+                    }else{
+                        if(background_slider > 0){
+                            background_slider -= 10;
+                        }
+                        else{
+                            background_slider = 0;
+                        }
+                    }
+
+                    sprite_off_x = 64;
+                    if(alternate_frame % 2){
+                        sprite_off_x = 128;
+                    }
+                    player.direction = LEFT;
+                }
+
+                if(player.x <= (screen_x - player.speed - player.dimensions) && (al_key_down(&ks, ALLEGRO_KEY_RIGHT) || al_key_down(&ks, ALLEGRO_KEY_D))){
+
+                    if(player.x < (screen_x - 200) || background_slider == (2048 - 456)){
+                        player.x += player.speed;
+                    }else{
+                        if(background_slider < (2048 - 456)){
+                            background_slider += 10;
+                        }
+                        else{
+                            background_slider = 2048 - 456;
+                        }
+                    }
+
+                    sprite_off_x = 64;
+                    if(alternate_frame % 2){
+                        sprite_off_x = 128;
+                    }
+                    player.direction = RIGHT;
+                }
 
                 if(al_key_down(&ks, ALLEGRO_KEY_SPACE) && jump){
                     ground_round = 0;
                     jump = false;
-                    frame = 50;
+                    frame = 60;
                 }
 
                 if(al_key_down(&ks, ALLEGRO_KEY_ESCAPE))
@@ -123,7 +174,7 @@ int main(){
                     player.y = level_height;
                     frame = 0;
                     ground_round++;
-                    if(ground_round > 3){
+                    if(ground_round > 1){
                         jump = true;
                     }
                 }
@@ -162,17 +213,27 @@ int main(){
 
         if(redraw && al_is_event_queue_empty(queue))
         {
+            if(player.direction == RIGHT){
+                sprite_off_y = 64;
+            }
+            else{
+                sprite_off_y = 0;
+            }
             ALLEGRO_VERTEX v[4];
             al_clear_to_color(al_map_rgb(0, 0, 0));
-            al_draw_scaled_bitmap(bkg, 0, 0, 4093, 2894, 0, 0, screen_x, screen_y, 0);
+            al_draw_scaled_bitmap(bkg, background_slider, 0, 456, 256, 0, 0, screen_x, screen_y, 0);
             al_draw_filled_ellipse(player.shadow_x, player.shadow_y, player.shadow_width, player.shadow_height, al_map_rgba_f(0, 0, 0, 0.5));
-            al_draw_scaled_bitmap(player.sprite, 0, 0, player.og_dimensions, player.og_dimensions, player.x, player.y, player.dimensions, player.dimensions, 0);
+            al_draw_scaled_bitmap(player.sprite, sprite_off_x, sprite_off_y, player.og_dimensions, player.og_dimensions, player.x, player.y, player.dimensions, player.dimensions, 0);
             al_draw_textf(font, al_map_rgb(255, 255, 255), 0, 0, 0, "X: %3.1f Y: %3.1f", player.x, player.y);
             
 
             al_flip_display();
             redraw = false;
         }
+        if(iteration % 4 == 0){
+            alternate_frame++;
+        }
+        iteration++;
     }
 
     al_destroy_bitmap(mysha);
